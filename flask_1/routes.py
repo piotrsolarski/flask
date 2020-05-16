16de6409a4379c8393048b18042f290c5860dd9b
piotrsolarski <piotrsolarsolarski@gmail.com>
2020-05-16 09:30:24 +0100

    posts added

--- flask_1/routes.py
+++ flask_1/routes.py
@@ -4,7 +4,7 @@ from PIL import Image
 from flask_1.users2 import get_users_comp
 from flask_1.models import User, Post
 from flask_1.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flask_1 import app, db, bcrypt
 from werkzeug.utils import secure_filename
 from flask_login import login_user, current_user, logout_user, login_required
@@ -118,7 +118,46 @@ def new_post():
         db.session.commit()
         flash('Your post has been created', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    form.title.data = post.title
+    form.content.data = post.content
+    return render_template('create_post.html', title="Update Post", form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('You post has been deleted', 'success')
+    return redirect(url_for('home'))
 
 
 @app.route('/upload', methods=['POST'])
@@ -141,7 +180,7 @@ def upload():
         return redirect(request.url)
 
 
-@app.route('/display/<filename>')
+@app.route('/display/<filename>', methods=['GET', 'POST'])
 def display_image(filename):
     # print('display_image filename: ' + filename)
     return redirect(url_for('static', filename='uploads/' + filename), code=301)
