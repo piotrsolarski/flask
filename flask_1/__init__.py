4f671cfc111215a4414bc98e8d751d471a249153
piotrsolarski <piotrsolarsolarski@gmail.com>
2020-05-16 10:30:25 +0100

    fixed

--- flask_1/__init__.py
+++ flask_1/__init__.py
@@ -3,15 +3,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f8824c95c795fc09db2c87f8aea49692'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///site.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///new.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
 
-from flask_1 import routes
+from flask_1 import routes, models
